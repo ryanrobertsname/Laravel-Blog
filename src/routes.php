b47c902e1059ee0668200c10d23483b9b3280d8e
Ryan Roberts <ryan@ryanroberts.name>
2015-01-07 11:17:52 -0800

    fix merge error for routes file

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,7 @@
 <?php
 
 // Main default listing e.g. http://domain.com/blog
-Route::get(Config::get('laravel-blog::routes.base_uri'), 'Fbf\LaravelBlog\PostsController@index');
+Route::get(Config::get('laravel-blog::routes.base_uri'), ['as' => 'laravel-blog.index', 'uses' => 'Fbf\LaravelBlog\PostsController@index']);
 
 // Archive (year / month / day) filtered listing e.g. http://domain.com/blog/yyyy/mm/dd
 Route::get(Config::get('laravel-blog::routes.base_uri').'/{year}/{month?}/{day?}', ['as' => 'laravel-blog.archive', 'uses' => 'Fbf\LaravelBlog\PostsController@indexByDate'])->where(array('year' => '\d{4}', 'month' => '\d{2}', 'day' => '\d{2}'));
@@ -12,8 +12,27 @@ if (Config::get('laravel-blog::routes.relationship_uri_prefix'))
 	Route::get(Config::get('laravel-blog::routes.base_uri').'/'.Config::get('laravel-blog::routes.relationship_uri_prefix').'/{relationshipIdentifier}', 'Fbf\LaravelBlog\PostsController@indexByRelationship');
 }
 
-// Blog post detail page e.g. http://domain.com/blog/my-post
-Route::get(Config::get('laravel-blog::routes.base_uri').'/{slug}', 'Fbf\LaravelBlog\PostsController@view');
+// Blog post detail variants based on date prefix config
+if (\Config::get('laravel-blog::routes.view_uri_date_prefix') == 'year')
+{
+	// Blog post detail page e.g. http://domain.com/blog/2015/my-post
+	Route::get(Config::get('laravel-blog::routes.base_uri').'/{year}/{slug}', ['as' => 'laravel-blog.viewWithYear', 'uses' => 'Fbf\LaravelBlog\PostsController@viewWithYear']);
+}
+else if (\Config::get('laravel-blog::routes.view_uri_date_prefix') == 'month') 
+{
+	// Blog post detail page e.g. http://domain.com/blog/2015/01/my-post
+	Route::get(Config::get('laravel-blog::routes.base_uri').'/{year}/{month}/{slug}', ['as' => 'laravel-blog.viewWithMonth', 'uses' => 'Fbf\LaravelBlog\PostsController@viewWithMonth']);
+}
+else if (\Config::get('laravel-blog::routes.view_uri_date_prefix') == 'day') 
+{
+	// Blog post detail page e.g. http://domain.com/blog/2015/01/02/my-post
+	Route::get(Config::get('laravel-blog::routes.base_uri').'/{year}/{month}/{day}/{slug}', ['as' => 'laravel-blog.viewWithDay', 'uses' => 'Fbf\LaravelBlog\PostsController@viewWithDay']);
+}
+else
+{
+	// Blog post detail page e.g. http://domain.com/blog/2015/01/my-post
+	Route::get(Config::get('laravel-blog::routes.base_uri').'/{slug}', ['as' => 'laravel-blog.viewWithSlug', 'uses' => 'Fbf\LaravelBlog\PostsController@viewWithSlug']);	
+}
 
 // RSS feed URL e.g. http://domain.com/blog.rss
 Route::get(Config::get('laravel-blog::routes.base_uri').'.rss', 'Fbf\LaravelBlog\PostsController@rss');
